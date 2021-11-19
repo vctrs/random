c9fc46529d125723c5d211c6e5d48291912c958f
vctrs <43434588+vctrs@users.noreply.github.com>
2021-11-19 11:04:01 +0100

    Update utils.js

--- utils.js
+++ utils.js
@@ -131,8 +131,8 @@ function Utils(errorOutputId) { // eslint-disable-line no-unused-vars
 
     this.startCamera = function(resolution, callback, videoId) {
         const constraints = {
-            'qvga': {width: {exact: 320}, height: {exact: 240}, facingMode: { exact: "environment" }},
-            'vga': {width: {exact: 640}, height: {exact: 480}, facingMode: {exact: "environment" }}
+            'qvga': {width: {exact: 320}, height: {exact: 240}, facingMode: { "environment" }},
+            'vga': {width: {exact: 640}, height: {exact: 480}, facingMode: { "environment" }}
         
         };
         let video = document.getElementById(videoId);
